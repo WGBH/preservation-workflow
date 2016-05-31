ab55977b243a206865c5d08ce74768fdb3e6d7fe
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-31 15:06:04 -0400

    Ignore zip files during spec diff: They will differ because of encoded creation dates.

--- spec/spec.sh
+++ spec/spec.sh
@@ -12,7 +12,7 @@ setup spec/fixtures/example-good/input
 
 # Expect success
 CI=true bash ./preserve.sh tmp/input tmp/output/metadata tmp/output/dest-1 tmp/output/dest-2
-diff -qr spec/fixtures/example-good/output tmp/output
+diff --exclude=*.zip -qr spec/fixtures/example-good/output tmp/output
 
 setup spec/fixtures/example-good/input
 
